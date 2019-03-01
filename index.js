c5ad2651e099e83ff4315b291a2128bf1b69dc2c
matwerber1 <matwerber@gmail.com>
2019-03-01 17:32:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-//Load express module with `require` directive
-var express = require('express');
+// This script starts a simple ExpressJS web server...
+// The server listens for HTTP on port 80 and HTTPS on port 443
+
+
+var express        = require('express');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var request        = new XMLHttpRequest();
 
-var request = new XMLHttpRequest();
-
-var fs = require('fs');
+var fs    = require('fs');
 var https = require('https');
 
 var port1 = 80;
 var port2 = 443;
 
-var app1 = express();
-var app2 = express();
+var app1 = express();   //app1 will listen for HTTP on port 80
+var app2 = express();   //app2 will listen for HTTPS on port 443
 
 var newline_regex = /\n/g;
 var html_tab = '&nbsp; &nbsp; &nbsp; &nbsp;';
 
-// The "false" makes this a synchronous request
-request.open('GET', 'http://169.254.170.2/v2/metadata', false);
+// Fargate containers provide metadata about the current ECS task 
+// at a special endpoint. We read this metadata below:
+request.open('GET', 'http://169.254.170.2/v2/metadata', false);   // The "false" makes this a synchronous request, rather than the default async
 request.send(null);
 
 if (request.status === 200) {
-  
   var ecsMetadata = JSON.parse(request.responseText);
   
 }
 
-//Define request response in root URL (/)
+// this is the handler for all HTTP requests received on port 80
 app1.get('/', function (req, res) {
   
   var msg = `Hello World from Express+NodeJS app1! Protocol is ${req.protocol} on port ${port1}.<br/><br/>ECS Metadata:<br/>${(JSON.stringify(ecsMetadata,null,2)).replace(newline_regex, "<br/>")}`;
@@ -34,12 +36,12 @@ app1.get('/', function (req, res) {
   
 });
 
-//Launch listening server
+//Enable the HTTP listener
 app1.listen(port1, function () {
   console.log(`app1 listening on port ${port1}!`);
 });
 
-//Define request response in root URL (/)
+// this is the handler for all HTTPS requests received on port 443
 app2.get('/', function (req, res) {
   
   var msg = `Hello World from Express+NodeJS app2! Protocol is ${req.protocol} on port ${port2}.<br/><br/>ECS Metadata:<br/>${(JSON.stringify(ecsMetadata,null,2)).replace(newline_regex, "<br/>")}`;
@@ -47,6 +49,10 @@ app2.get('/', function (req, res) {
   
 });
 
+// Enable the HTTPS listener
+// Because its HTTPS, we must specify the SSL key and certificate
+// Normally, these shouldn't be stored in a public repo, but this is ok for test purposes
+// Because I don't use these certs for anything else
 https.createServer({
   key: fs.readFileSync('server.key'),
   cert: fs.readFileSync('server.cert')
